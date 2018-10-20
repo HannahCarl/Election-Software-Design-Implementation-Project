82270d6f5be8c5fef81e087a702129b8f5ff7fe3
MarshallRich <rlmarsh@g.clemson.edu>
2018-10-20 19:06:47 -0400

    fixed phone numbers in voters and stored voter in voting session
    added cellPhone and homePhone to voter to replace phoneNumber object, and added a method to voting session to store a voter's info while they are logged in for later use when casting a vote.

--- TeamWhy-MidtermElectionApp/src/com/csci360/electionapp/model/VotingSession.java
+++ TeamWhy-MidtermElectionApp/src/com/csci360/electionapp/model/VotingSession.java
@@ -3,7 +3,18 @@ package com.csci360.electionapp.model;
 public class VotingSession {
     private Voter voter;
 
-    public boolean VotingSession(String voterID, String socSecNumber) {
-        return true;
+    public VotingSession() {
+
+    }
+
+    public boolean voterLogin(String voterID, String socSecNum, VoterList voterList) {
+        if (voterList.checkForVoter(voterID, socSecNum) == true) {
+            this.voter = voterList.getVoterByLoginInfo(voterID, socSecNum);
+
+            return true;
+        }
+
+
+        return false;
     }
 }
