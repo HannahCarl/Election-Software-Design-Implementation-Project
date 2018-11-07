77dab52c6acbf0858ad5e1f76c230969bedff896
Hannah Posch <poschhn@g.cofc.edu>
2018-11-07 08:38:34 -0500

    Added GRASP pattern comments

--- TeamWhy-MidtermElectionApp/src/com/csci360/electionapp/model/RegistrantList.java
+++ TeamWhy-MidtermElectionApp/src/com/csci360/electionapp/model/RegistrantList.java
@@ -5,10 +5,14 @@ import java.util.ArrayList;
 public class RegistrantList {
     private ArrayList<Registrant> registeredPersonList;
 
+    //RegistrantList - Creator (GRASP)
+    //This constructor method creates a new Registrant List.
     public RegistrantList() {
         this.registeredPersonList = new ArrayList<Registrant>();
     }
-
+    
+    //CheckSocial - Information Expert (GRASP)
+    //This method checks to see if the social security number provided is a duplicate with another on registeredPersonList.
     public boolean checkSocial(String social) {
         boolean socialIsDupe = false;
 
@@ -21,7 +25,8 @@ public class RegistrantList {
         }
         return socialIsDupe;
     }
-
+    //CheckForRegistrant - Information Expert (GRASP)
+    //This method checks to see if the registrant is a duplicate with another on registeredPersonList.
     public boolean checkForRegistrant(String firstName, String lastName, String socSecNum) {
         boolean registrantExists = false;
 
@@ -36,6 +41,8 @@ public class RegistrantList {
         return registrantExists;
     }
 
+    //GetRegistrantByLoginInfo - Information Expert (GRASP)
+    //This method returns the correct registrant based on login info provided.
     public Registrant getRegistrantByLoginInfo(String firstName, String lastName, String socSecNum) {
         Registrant desiredRegistrant = null;
 
@@ -50,6 +57,8 @@ public class RegistrantList {
         return desiredRegistrant;
     }
 
+    //AddRegistrant - Low Coupling (GRASP)
+    //This method adds a new registrant to the registeredPersonList.
     public void addRegistrant(Registrant registrant) {
     	registeredPersonList.add(registrant);
     }
